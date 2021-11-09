e4a5aa65bbbb47a1a4535520a3ed2740898ac9c9
Duffie Cooley <dcooley@isovalent.com>
2021-11-09 08:43:15 -0800

    updated
    Signed-off-by: Duffie Cooley <dcooley@isovalent.com>

--- down.sh
+++ down.sh
@@ -1,1 +1,2 @@
-gcloud compute instances delete mke-dev
+gcloud compute instances delete $(whoami)-vagrant-dev
+#gcloud compute config-ssh --remove
--- up.sh
+++ up.sh
@@ -1,1 +1,2 @@
-gcloud compute instances create mke-dev   --enable-nested-virtualization   --zone=us-west1-a   --min-cpu-platform="Intel Haswell" --labels=owner=dcooley --machine-type=n1-standard-32 --image-project=ubuntu-os-cloud --image-family=ubuntu-2004-lts --boot-disk-size=200GB
+gcloud compute instances create $(whoami)-vagrant-dev   --enable-nested-virtualization   --zone=us-west1-a   --min-cpu-platform="Intel Haswell" --labels=owner=$(whoami) --machine-type=n1-standard-32 --image-project=ubuntu-os-cloud --image-family=ubuntu-2004-lts --boot-disk-size=200GB
+gcloud compute config-ssh 
